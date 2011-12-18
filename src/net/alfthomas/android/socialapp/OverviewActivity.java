4b092bde2a2fcf76d17dbd94f17756168864feef
alfthomas <alf.thomas.nilsen@gmail.com>
2011-12-18 15:38:21 +0100

    Created main menu with preferences item

--- src/net/alfthomas/android/socialapp/OverviewActivity.java
+++ src/net/alfthomas/android/socialapp/OverviewActivity.java
@@ -2,12 +2,29 @@ package net.alfthomas.android.socialapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class OverviewActivity extends Activity {
-    /** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = this.getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, "Just a test", Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
